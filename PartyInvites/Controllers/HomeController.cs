1abaf4972d6d8bf133631d63f9ea0ac96b7ef5dd
Morpheus <Morpheus@PROMETHEUS>
2016-12-17 13:48:23 -0700

    Add project files.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ActionResult Index()
+        {
+            int h = DateTime.Now.Hour;
+            ViewBag.Greetings = h < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+         
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RsvpForm(GuestResponse guestResponse)
+        {
+            return View("Thanks",guestResponse);
+        }
+    }
+}
+
